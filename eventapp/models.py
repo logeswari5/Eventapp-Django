9939d8c20ac8443731f9126c2e19fea6b9964a33
HP <logeswari5>
2021-04-20 15:11:05 +0530

    Add event time model in models file

--- eventapp/models.py
+++ eventapp/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-
-from django.db import models
 from django.urls import reverse
 from model_utils.models import TimeStampedModel
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
-
+from datetime import date
 from places.models import Place
+from django.utils import timezone
 
 
 class Event(TimeStampedModel):
@@ -14,7 +13,20 @@ class Event(TimeStampedModel):
     host = models.ForeignKey(User,on_delete=models.SET_NULL, null=True)
     tags = TaggableManager()
     place = models.ForeignKey(Place, on_delete=models.SET_NULL, null=True, blank=True)
-
+    event_time = models.DateTimeField(default=timezone.now, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
     def __str__(self):
         return '{0}'.format(self.title, self.host)
 
+    def get_absolute_url(self):
+        return reverse('event-detail', args=[str(self.id)])
+
+class EventTime(models.Model):
+    event = models.ForeignKey('Event',on_delete=models.SET_NULL,null=True)
+    date = models.DateField(default=date.today)
+    start_time = models.TimeField(blank=True,null=True)
+    end_time = models.TimeField(blank=True,null=True)
+
+    def __str__(self):
+        return str(self.date)
