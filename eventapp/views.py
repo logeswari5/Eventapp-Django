f18fb33fdd682643beddc3b6cbce6e64373ca19a
HP <logeswari5>
2021-04-20 15:16:25 +0530

    Create list view

--- eventapp/views.py
+++ eventapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html',)
+
+class EventListView(generic.ListView):
+    model = Event
+    paginate_by = 2
+
